f27df6c0b12a8da10e01599c9e54c3632df0f640
perwaaler <perwaaler@gmail.com>
2022-10-25 17:59:00 +0200

    got rmse of 0.603 using p=1

--- src/training.py
+++ src/training.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import yaml
 import os
 import io
 import sys
+from sklearn.metrics import mean_squared_error
+import math
 
 # get the parameter for the training stage:
-params = yaml.safe_load(open("parameters/params.yaml"))['training'] # training is the name of the workspace variable
+params = yaml.safe_load(open("parameters/params.yaml"))['training']
 # get the path to the data. Note: sys.argv contains a list of the command line arguments used when running a commands
 data_path = sys.argv[1]+"/out.csv"
 # read output from the filter stage:
@@ -13,7 +17,7 @@ df = pd.read_csv(data_path)
 # get the number of rows specified by the num_rows parameter:
 df = df.head(params['num_rows'])
 print("taking the mean of:")
-print(df)
+
 # get the mean of the y-column
 mean = df['y'].mean()
 print(mean)
@@ -22,6 +26,22 @@ print(mean)
 os.makedirs(os.path.join("output","training"), exist_ok=True)
 # create name (with path) of output:
 output_training = os.path.join("output","training","out.csv")
+
+### linear regression ###
+p = 1
+print(p)
+x = df.x1
+A = np.vstack([x, x**p, np.ones(len(x))]).T
+y = df.y
+c = np.linalg.lstsq(A, y)[0]
+y_pred = A @ c.T
+e = y_pred - y
+
+MSE = mean_squared_error(y, y_pred)
+RMSE = math.sqrt(MSE)
+
+print("the RMSE of the fit is:")
+print(RMSE)
 
 with io.open(output_training,"w",encoding="utf8") as fd_out_training:
     fd_out_training.write(str(mean))
